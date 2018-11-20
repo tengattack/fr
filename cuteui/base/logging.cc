55918a3e3e9ba93a503fe15c60d0116df5a6f7ec
icylight <408209435@qq.com>
2018-11-20 19:40:16 +0800

    add callback in logging

--- cuteui/base/logging.cc
+++ cuteui/base/logging.cc
@@ -97,7 +97,9 @@ const char* log_severity_name(int severity) {
   return "UNKNOWN";
 }
 // 自行添加的
-std::shared_ptr<nlohmann::crow> g_sentry_client;
+void EmptyCb(std::string log, int severity, void* user_data) { return; }
+LoggingSettings::Callback g_callback = EmptyCb;
+void* g_user_data = nullptr;
 
 int g_min_log_level = 0;
 
@@ -379,7 +381,7 @@ LoggingSettings::LoggingSettings()
       delete_old(APPEND_TO_OLD_LOG_FILE) {}
 
 bool BaseInitLoggingImpl(const LoggingSettings& settings) {
-  g_sentry_client = settings.sentry_client;
+  g_callback = nullptr == settings.callback ? EmptyCb : settings.callback;
 #if defined(OS_NACL)
   // Can log only to the system debug log.
   CHECK_EQ(settings.logging_dest & ~LOG_TO_SYSTEM_DEBUG_LOG, 0);
@@ -770,15 +772,7 @@ LogMessage::~LogMessage() {
     }
   }
 
-  bool need_send_to_sentry = (severity_ >= LOG_ERROR);
-#ifdef _DEBUG
-  need_send_to_sentry = true;
-#endif
-  if (need_send_to_sentry && nullptr != logging::g_sentry_client) {
-    size_t start = str_newline.find(':');
-    logging::g_sentry_client->capture_message(std::string("[") +
-                                              str_newline.substr(start + 1));
-  }
+  g_callback(str_newline, severity_, g_user_data);
 
   if (severity_ == LOG_FATAL) {
     // Write the log message to the global activity tracker, if running.
