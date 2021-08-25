468bd2b4f37e28b88a6b4bed210e07997bbd6656
Strive-Sun <1096235292@qq.com>
2021-08-25 15:26:08 +0800

    添加创建日志和删除文件代码

--- cuteui/base/file/file.h
+++ cuteui/base/file/file.h
@@ -31,6 +31,7 @@ namespace base{
 		}
 		bool OpenW(unsigned long mode, LPCWSTR lpszPath);
 		bool OpenA(unsigned long mode, LPCSTR lpszPath);
+        bool OpenLog(unsigned long mode, LPCWSTR lpszPath);
 
 		bool IsOpen();
 		void Close();
@@ -41,6 +42,7 @@ namespace base{
 		bool SetPointer(uint64 offset, unsigned long dwMoveMethod = FILE_BEGIN);
 
 		bool Flush();
+        bool Delete(LPCWSTR lpszPath);
 
 		static inline bool Delete(LPCTSTR lpszPath) {
 #ifdef _UNICODE
@@ -54,6 +56,7 @@ namespace base{
 
 	protected:
 		bool m_opened;
+        bool m_deleted;
 		HANDLE m_hFile;
 		unsigned long m_mode;
 
