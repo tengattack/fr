91ae4da10c70a12aa90328e966dc6b59c65378df
sunjinlei <sunjinlei@bilibili.com>
2023-05-11 18:13:38 +0800

    fix

--- cuteui/base/file/file.cc
+++ cuteui/base/file/file.cc
@@ -88,14 +88,14 @@ bool CFile::DeleteA(LPCSTR lpszPath)
 
 bool CFile::ExistsW(LPCWSTR lpszPath) {
 	DWORD attrib = GetFileAttributesW(lpszPath);
-	return (attrib != INVALID_FILE_ATTRIBUTES &&
-			(!(attrib & FILE_ATTRIBUTE_DIRECTORY)));
+	return !(attrib == INVALID_FILE_ATTRIBUTES &&
+			GetLastError() == ERROR_FILE_NOT_FOUND);
 }
 
 bool CFile::ExistsA(LPCSTR lpszPath) {
 	DWORD attrib = GetFileAttributesA(lpszPath);
-	return (attrib != INVALID_FILE_ATTRIBUTES &&
-			(!(attrib & FILE_ATTRIBUTE_DIRECTORY)));
+	return !(attrib == INVALID_FILE_ATTRIBUTES &&
+			GetLastError() == ERROR_FILE_NOT_FOUND);
 }
 
 bool CFile::OpenW(unsigned long mode, LPCWSTR lpszPath)
