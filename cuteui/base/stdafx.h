8eca1bf549b16eedcff9c017d4b4e95b97fece0a
sunjinlei <sunjinlei@bilibili.com>
2023-05-22 15:30:03 +0800

    添加新文件

---
+++ cuteui/base/stdafx.h
@@ -0,0 +1,15 @@
+// Copyright (C) 2018 MiaoSiLa Corporation. All rights reserved.
+
+#ifndef MISSEVAN_FM_KERNEL_STDAFX_H_
+#define MISSEVAN_FM_KERNEL_STDAFX_H_
+
+#include <WinSock2.h>
+#include <Windows.h>
+#include <stdio.h>
+#include <tchar.h>
+
+#include "targetver.h"
+
+// TODO:  在此处引用程序需要的其他头文件
+
+#endif  // MISSEVAN_FM_KERNEL_STDAFX_H_
