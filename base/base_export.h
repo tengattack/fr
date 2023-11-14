7896385aa09664fa352d8c46cd4e6600d7ff101e
Strive-Sun <1096235292@qq.com>
2023-11-14 10:40:05 +0800

    add FR_EXPORT

--- base/base_export.h
+++ base/base_export.h
@@ -1,6 +1,9 @@
-
+#if defined(FR_EXPORT)
 #if defined(WIN32)
 #define BASE_EXPORT __declspec(dllexport)
 #else
-#define BASE_EXPORT __declspec(dllimport)
+#define BASE_EXPORT __attribute__((visibility("default")))
+#endif
+#else
+#define BASE_EXPORT
 #endif
